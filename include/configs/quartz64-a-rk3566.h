207079c45447528e3024a6bed4d1afd1bb112136
Peter Geis <pgwipeout@gmail.com>
2021-12-19 18:57:36 -0500

    rockchip: rk356x: add quartz64-a board
    Signed-off-by: Peter Geis <pgwipeout@gmail.com>

---
+++ include/configs/quartz64-a-rk3566.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __QUARTZ64_A_RK3566_H
+#define __QUARTZ64_A_RK3566_H
+
+#include <configs/rk3568_common.h>
+
+#define CONFIG_SUPPORT_EMMC_RPMB
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+			"stdout=serial,vidconsole\0" \
+			"stderr=serial,vidconsole\0"
+
+#endif
