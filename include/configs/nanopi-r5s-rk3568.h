73e217fa2e3be0b69248ead171363eda4518a660
Marty Jones <mj8263788@gmail.com>
2023-04-15 02:04:31 -0400

    uboot: rockchip: add Nano Pi R5S
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

---
+++ include/configs/nanopi-r5s-rk3568.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __NANOPI_R5S_RK3568_H
+#define __NANOPI_R5S_RK3568_H
+
+#include <configs/rk3568_common.h>
+
+#define CONFIG_SUPPORT_EMMC_RPMB
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+			"stdout=serial,vidconsole\0" \
+			"stderr=serial,vidconsole\0"
+
+#define CONFIG_USB_OHCI_NEW
+#define CONFIG_SYS_USB_OHCI_MAX_ROOT_PORTS     2
+
+#endif
